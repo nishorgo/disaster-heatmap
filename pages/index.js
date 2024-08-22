5b26b64674e42aa90b57aa3c66e4f0da247d95cb
Abid Hasan <abidhasan.bh@outlook.com>
2024-08-23 00:45:52 +0600

    google credentials updated

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,21 @@ import { useState } from "react";
 
 
 export async function getServerSideProps() {
-  const auth = await gl.auth.getClient({ scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"] });
+  const credentials = {
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+    universe_domain: process.env.UNIVERSE_DOMAIN
+  };
+
+  const auth = await gl.auth.getClient({ credentials, scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"] });
   const sheets = gl.sheets({ version: 'v4', auth });
 
   const range = "Sheet1!A2:C10000";
@@ -25,7 +39,7 @@ export async function getServerSideProps() {
 
 export default function Home({ locations }) {
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: process.env.MAPS_API_KEY,
+    googleMapsApiKey: "AIzaSyCz2ghAQaQ3qX0OiaTUNZfgHFHLYD4yaXs",
     libraries: ["visualization"]
   });
   const [map, setMap] = useState(null);
@@ -34,7 +48,7 @@ export default function Home({ locations }) {
     return <div className="grid place-items-center h-screen text-xl">Loading... please wait.</div>
   }
   
-  const center = {lat: 37.782, lng: -122.447};
+  const center = {lat: 22.8724, lng: 91.0973};
   const heatMapData = locations.map((location) => ({
     location: new google.maps.LatLng(location[0], location[1]), weight: location[2]
   }));
@@ -46,7 +60,7 @@ export default function Home({ locations }) {
       <GoogleMap 
         mapContainerStyle={{position: "relative", width: "100%", height: "100%"}}
         center={center}
-        zoom={15}
+        zoom={10}
         onLoad={(map) => setMap(map)}
       >
         { map && heatMapData &&
